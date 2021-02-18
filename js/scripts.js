dae8461f79ca107ec9a346cb85611e6333f46062
lgude <73794081+lgude@users.noreply.github.com>
2021-02-18 12:03:28 -0600

    update any errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-console.log("Hello! Welcome to my Website!");
+console.log('Hello! Welcome to my Website!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
-}
+};
+menuToggle()
